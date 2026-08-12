d141167ac670f73b8ff4a0c85792fc112e90d2a6
thiagosjcx <thisaint07@gmail.com>
2026-08-12 20:21:05 -0300

    Questao 6 - numero desaparecido

---
+++ questao6/program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Questao6;
+
+Console.WriteLine("Digite os números separados por espaço (ex: 3 0 1):");
+
+int[] numeros = (Console.ReadLine() ?? "")
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int resultado = NumeroDesaparecido.Resolver(numeros);
+
+Console.WriteLine($"Número que falta: {resultado}");
---
+++ questao6/questao6.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Questao6;
+
+public static class NumeroDesaparecido
+{
+    // Usa a soma de Gauss: soma esperada (0 até n) menos a soma real
+    // do array = o número que falta. Sem ordenar, sem estrutura extra.
+    // Complexidade: O(n) de tempo, O(1) de espaço extra.
+    public static int Resolver(int[] numeros)
+    {
+        int n = numeros.Length; // array tem n elementos, mas vai de 0 a n
+        int somaEsperada = n * (n + 1) / 2;
+        int somaReal = numeros.Sum();
+        return somaEsperada - somaReal;
+    }
+}
