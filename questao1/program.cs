fed6a1c5ec9a40e4dd47398a6276d64f99540f30
thiagosjcx <thisaint07@gmail.com>
2026-08-12 19:04:14 -0300

    Questao 1 - soma de pares

---
+++ questao1/program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Questao1;
+
+Console.WriteLine("Digite números separados por espaços");
+
+string? entrada = Console.ReadLine();
+
+int[] numeros = (entrada ?? "")
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int resultado = SomaPares.Resolver(numeros);
+Console.WriteLine($"A soma dos números pares é: {resultado}");
---
+++ questao1/questao1.cs
@@ -0,0 +1,21 @@
+﻿namespace Questao1;
+
+public static class SomaPares
+{
+    public static int Resolver(int[] numeros)
+    {
+        int soma = 0;
+
+        foreach (int numero in numeros)
+        {
+            bool eImpar = numero % 2 != 0;
+
+            if (eImpar)
+                continue;
+
+            soma += numero;
+        }
+
+        return soma;
+    }
+}
