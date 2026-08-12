7f67bb3b53e33ea7e217865e8ca61fbe4c4aafac
thiagosjcx <thisaint07@gmail.com>
2026-08-12 19:22:40 -0300

    Questao 2 - segundo maior numero; remove bin/obj/.vs do controle de versao

---
+++ questao2/program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Questao2;
+
+Console.WriteLine("Digite números separados por espaços:");
+
+string? entrada = Console.ReadLine();
+
+int[] numeros = (entrada ?? "")
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int? resultado = SegundoMaior.Resolver(numeros);
+
+if (resultado == null)
+    Console.WriteLine("Erro: não existe um segundo maior número distinto nesse array.");
+else
+    Console.WriteLine($"Segundo maior número: {resultado}");
---
+++ questao2/questao2.cs
@@ -0,0 +1,26 @@
+﻿namespace Questao2;
+
+public static class SegundoMaior
+{
+    public static int? Resolver(int[] numeros)
+    {
+        int? maior = null;
+        int? segundoMaior = null;
+
+        foreach (int numero in numeros)
+        {
+            if (maior == null || numero > maior)
+            {
+                if (numero != maior)
+                    segundoMaior = maior;
+                maior = numero;
+            }
+            else if (numero != maior && (segundoMaior == null || numero > segundoMaior))
+            {
+                segundoMaior = numero;
+            }
+        }
+
+        return segundoMaior;
+    }
+}
