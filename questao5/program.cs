8021de0a23e05a5cbc26a427f1fb3181b3fd1d0b
thiagosjcx <thisaint07@gmail.com>
2026-08-12 19:58:31 -0300

    Questao 5 - contagem de caracteres

---
+++ questao5/program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Questao5;
+
+Console.WriteLine("Digite uma palavra (ex: banana):");
+
+string palavra = Console.ReadLine() ?? "";
+
+var resultado = ContagemCaracteres.Resolver(palavra);
+
+foreach (var par in resultado)
+    Console.WriteLine($"{par.Key}: {par.Value}");
---
+++ questao5/questao5.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Questao5;
+
+public static class ContagemCaracteres
+{
+    public static Dictionary<char, int> Resolver(string palavra)
+    {
+        var contagem = new Dictionary<char, int>();
+
+        foreach (char letra in palavra)
+        {
+            if (contagem.ContainsKey(letra))
+                contagem[letra]++;
+            else
+                contagem[letra] = 1;
+        }
+
+        return contagem;
+    }
+}
