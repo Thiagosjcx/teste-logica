77fb509da379bd12370473e3fdd2c5886861f49f
thiagosjcx <thisaint07@gmail.com>
2026-08-12 19:53:16 -0300

    Questao 4 - dois numeros (two sum)

---
+++ questao4/program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Questao4;
+
+Console.WriteLine("Digite o target:");
+int target = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Digite os números separados por espaço (ex: 2 7 11 15):");
+int[] numeros = (Console.ReadLine() ?? "")
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[]? resultadoForcaBruta = TwoSum.ForcaBruta(numeros, target);
+int[]? resultadoOtimizado = TwoSum.Otimizado(numeros, target);
+
+Console.WriteLine(resultadoForcaBruta != null
+    ? $"Força bruta -> índices: [{string.Join(", ", resultadoForcaBruta)}]"
+    : "Força bruta -> nenhum par encontrado.");
+
+Console.WriteLine(resultadoOtimizado != null
+    ? $"Otimizado   -> índices: [{string.Join(", ", resultadoOtimizado)}]"
+    : "Otimizado   -> nenhum par encontrado.");
---
+++ questao4/questao4.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Questao4;
+
+public static class TwoSum
+{
+    public static int[]? ForcaBruta(int[] numeros, int target)
+    {
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            for (int j = i + 1; j < numeros.Length; j++)
+            {
+                if (numeros[i] + numeros[j] == target)
+                    return new[] { i, j };
+            }
+        }
+        return null;
+    }
+
+    public static int[]? Otimizado(int[] numeros, int target)
+    {
+        var jaVistos = new Dictionary<int, int>(); 
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            int complemento = target - numeros[i];
+
+            if (jaVistos.TryGetValue(complemento, out int indice))
+                return new[] { indice, i };
+
+            jaVistos[numeros[i]] = i;
+        }
+
+        return null;
+    }
+}
